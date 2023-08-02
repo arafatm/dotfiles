ea9bb07b804f73ec9702b626563574d3e13738e9
Arafat M <19172+arafatm@users.noreply.github.com>
2023-08-01 21:31:41 -0700

    clean

--- home/bin/open_mailto.sh
+++
@@ -1,2 +0,0 @@
-notify-send -i mail 'GMailing' `echo $1 | sed 's/mailto://'`
-/usr/bin/sensible-browser -remote "openurl(https://mail.google.com/mail?view=cm&tf=0&to=`echo $1 | sed ’s/mailto://’`,new-tab)"
