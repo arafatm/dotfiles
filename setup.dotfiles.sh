3ddaadae4480aa294bd1d9d80db597e78f89678b
Arafat M <19172+arafatm@users.noreply.github.com>
2023-08-05 14:56:55 -0700

    Commit All

--- setup.dotfiles.sh
+++ setup.dotfiles.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 set -e
 
-echo "***** ***** Setup dotfiles***** ***** "
+echo '########## Setting up dotfiles ########## '
 DOTFILES=$HOME/code/dotfiles
 
 echo "*** Setup dotfiles"
 
-echo "## Setting up ssh"
-if [[ ! -f "$HOME/.ssh/ssh.github" ]]; then
-  echo "ssh keys not found"
+echo '---------- Setting up ssh ---------- '
+if [[ ! -f "$HOME/.ssh/arafatm.ssh" ]]; then
+  echo "arafatm.ssh key not found"
   exit
 else
-  eval `keychain --agents ssh --eval ssh.github`
+  eval `keychain --agents ssh --eval arafatm.ssh`
 fi
 
-echo "## Setting up oh-my-zsh"
+echo '---------- Setting up oh-my-zsh ----------'
 rm -rf $HOME/.oh-my-zsh && \
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --keep-zshrc --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/.oh-my-zsh/custom/themes/powerlevel10k
 echo "- installed ohmyzsh"
 
-echo "## Setting up dotfiles"
+echo '---------- Setting up dotfiles ----------'
 for f in `ls -A $DOTFILES/home`; do
   rm -rf $HOME/$f
   ln -s $DOTFILES/home/$f $HOME/
@@ -32,7 +32,7 @@ if [ ! -d "$HOME/tmp" ]; then
 fi
 echo '- created ~/tmp'
 
-echo "## Setting up NeoVim"
+echo '---------- Setting up NeoVim ----------'
 rm -rf $HOME/.config/nvim
 mkdir -p $HOME/.config/nvim
 for f in `ls -A $DOTFILES/nvim`; do
@@ -40,15 +40,14 @@ for f in `ls -A $DOTFILES/nvim`; do
 done
 echo '- created neovim'
 
-#echo "## Switch to zsh"
+#echo '---------- Switch to zsh ----------'
 #if [ $(echo $SHELL) != "/bin/zsh" ]; then chsh -s $(which zsh); fi
 #echo "shell should be zsh"
 
-#echo "## unalias and source .zshrc"
+#echo '---------- unalias and source .zshrc ----------'
 #unalias -a; source $HOME/.zshrc
 
-echo "*** DONE Setup dotfiles ***"
+echo '---------- DONE Setup dotfiles ----------'
 
-echo "############################################################"
-echo "chsh -s zsh"
+echo '$ chsh -s zsh'
 
