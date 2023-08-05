e859ab361a3c7a46246f30498e74bf3984ec2fab
Arafat M <19172+arafatm@users.noreply.github.com>
2023-08-05 14:26:02 -0700

    setup.ubuntu.sh

--- setup.ubuntu.sh
+++ setup.ubuntu.sh
@@ -5,7 +5,7 @@ set -e
 DOTFILES=$HOME/code/dotfiles
 
 if [ ! -f /etc/lsb-release ]; then
-  echo "-----\nNot Ubuntu\n-----"
+  echo '-----\nNot Ubuntu\n-----'
   exit
 fi
 
@@ -26,12 +26,13 @@ else
 fi
 
 if [[ $(HOST) == "kodi" ]]; then
-  echo "############################################################"
-  echo "ssh-copy-id -i <ssh-file> kodi@hostname" 
-  echo "cat GH_TOKEN={GH Kodi Token} > $HOME/.env"
+  echo '############################################################'
+  echo 'ssh-copy-id -i <ssh-file> kodi@hostname' 
+  echo 'cat GH_TOKEN={GH Kodi Token} > $HOME/.env'
+  echo '0 0 * * * docker image prune'
+  echo '0 * * * * echo "##### $(date) $(HOME/duck.sh) #####" >> $LOG 2>&1'
 fi
 
-echo "############################################################"
-echo "## If not primary (e.g. Kodi)..."
-echo "0 0 * * * rm -rf $HOME/code/dotfiles && git clone  https://github.com/arafatm/dotfiles.git $HOME/dotfiles"
-echo "############################################################"
+echo '############################################################'
+echo '0 * * * * $HOME/bin/bkps.git.cron >> $LOG 2>&1'
+echo '############################################################'
